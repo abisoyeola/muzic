2830c7b072b23eb77357acddc5a9ad816cd28f2f
Idris Abass <olaabisoye@gmail.com>
2024-08-23 14:29:36 +0100

    map

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,16 +7,180 @@ import as from './img/as.png'
 import waves from './img/wave.png'
 
 const Home = () => {
+
+    const musicList = [
+        {
+            id: 1,
+            songname: 'Bloody Samaritan',
+            artistname: 'Ayra Starr',
+            cardBG: {
+                backgroundImage: "url(/musicimg/as.png)",
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 200,
+                width: 150,
+                borderRadius: 20,
+                display: 'flex',
+                marginRight: 40,
+            },
+            cardBgColor: {
+                backgroundImage: 'linear-gradient(rgba(255,255,0,0), black)',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 200,
+                width: 150,
+                borderRadius: 20,
+                display: 'flex',
+                flexDirection: 'column',
+                color: 'white'
+            }
+        },
+        {
+            id: 2,
+            songname: 'Bad Influence',
+            artistname: 'Omah Lay',
+            cardBG: {
+                backgroundImage: "url(/musicimg/ol.jpeg)",
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 200,
+                width: 150,
+                borderRadius: 20,
+                display: 'flex',
+                marginRight: 40,
+            },
+            cardBgColor: {
+                backgroundImage: 'linear-gradient(rgba(255,255,0,0), lemon)',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 200,
+                width: 150,
+                borderRadius: 20,
+                display: 'flex',
+                flexDirection: 'column',
+                color: 'white'
+            }
+        },
+        {
+            id: 3,
+            songname: 'Essence',
+            artistname: 'WizKid',
+            cardBG: {
+                backgroundImage: "url(/musicimg/wk.jpeg)",
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 300,
+                width: 200,
+                borderRadius: 20,
+                display: 'flex',
+                marginRight: 40,
+            },
+            cardBgColor: {
+                backgroundImage: 'linear-gradient(rgba(255,255,0,0), black)',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 300,
+                width: 200,
+                borderRadius: 20,
+                display: 'flex',
+                flexDirection: 'column',
+                color: 'white'
+            }
+        },
+        {
+            id: 4,
+            songname: 'Standing Ovation',
+            artistname: 'Tiwa Savage',
+            cardBG: {
+                backgroundImage: "url(/musicimg/tw.jpeg)",
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 200,
+                width: 150,
+                borderRadius: 20,
+                display: 'flex',
+                marginRight: 40,
+            },
+            cardBgColor: {
+                backgroundImage: 'linear-gradient(rgba(255,255,0,0), blue)',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 200,
+                width: 150,
+                borderRadius: 20,
+                display: 'flex',
+                flexDirection: 'column',
+                color: 'white'
+            }
+        },
+        {
+            id: 5,
+            songname: 'Yewa',
+            artistname: 'Kizz Daniel',
+            cardBG: {
+                backgroundImage: "url(/musicimg/kd.jpeg)",
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 200,
+                width: 150,
+                borderRadius: 20,
+                display: 'flex',
+                marginRight: 40,
+            },
+            cardBgColor: {
+                backgroundImage: 'linear-gradient(rgba(255,255,0,0), black)',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 200,
+                width: 150,
+                borderRadius: 20,
+                display: 'flex',
+                flexDirection: 'column',
+                color: 'white'
+            }
+        },
+        {
+            id: 6,
+            songname: 'Gwagwalada',
+            artistname: 'Buju',
+            cardBG: {
+                backgroundImage: "url(/musicimg/kd.jpeg)",
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 200,
+                width: 150,
+                borderRadius: 20,
+                display: 'flex',
+                marginRight: 40,
+            },
+            cardBgColor: {
+                backgroundImage: 'linear-gradient(rgba(255,255,0,0), black)',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                height: 200,
+                width: 150,
+                borderRadius: 20,
+                display: 'flex',
+                flexDirection: 'column',
+                color: 'white'
+            }
+        }
+    ]
+
+
     return (
         <div style={styles.content}>
             <div style={styles.card}>
                 <ActiveButton btn={styles.btnwest} content={<img src={back} width={24} />} />
 
-                <Card songname={'Bloody Samaritan'} artistname={'Ayra Starr'} imagestyle={styles.cardbg} colorstyle={styles.cardbgcolor} />
-                <Card songname={'Bad Influence'} artistname={'Omah Lay'} imagestyle={styles.cardbg} colorstyle={styles.cardbgcolor} />
-                <Card songname={'Essence'} artistname={'Wizkid'} imagestyle={styles.cardbg} colorstyle={styles.cardbgcolor} />
-                <Card songname={'Standing Ovation'} artistname={'Tiwa Savage'} imagestyle={styles.cardbg} colorstyle={styles.cardbgcolor} />
-                <Card songname={'Yewah'} artistname={'Kizz Daniel'} imagestyle={styles.cardbg} colorstyle={styles.cardbgcolor} />
+                {
+                    musicList.map((music) => (
+                        music.id <= 5 && <Card songname={music.songname} artistname={music.artistname} imagestyle={music.cardBG} colorstyle={music.cardBgColor} />
+                    ))
+                }
+
+
+
 
                 <ActiveButton btn={styles.btneast} content={<img src={fwd} width={24} />} />
             </div>
@@ -52,6 +216,7 @@ const styles = {
     },
     btnwest: {
         padding: 10,
+        marginRight: 40,
         color: 'blue',
         width: 50,
         height: 50,
